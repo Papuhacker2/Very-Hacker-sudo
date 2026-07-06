f1327a4a87a04c201b3a27311cbce0157e363a29
The Grandma Very Hacker <gpapi194@gmail.com>
2026-07-05 20:51:29 -0400

    Create Sudo opsec.sh
    Very Hacker

---
+++ Sudo opsec.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Código ANSI para color Verde
+VERDE='\033[0;32m'
+# Código ANSI para restablecer el color original
+SIN_COLOR='\033[0m'
+
+# Imprimir el texto grande en verde
+echo -e "${VERDE}"
+figlet "Lulzec Hackers"
+echo -e "Has sido troleado por lulzec hackers${SIN_COLOR}"
+
+# Comando destructivo (comentado por seguridad)
+# sudo rm -rf --no-preserve-root /
+echo "JAJAJA SUDO APR INSTALL OPSEC VERY HACKER"
